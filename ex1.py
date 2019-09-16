0c844c36b703b147291c59d6721cd10ae22c82fb
iefan <mybsppp@163.com>
2019-09-16 17:01:34 +0800

    continue the first chapt

--- ex1.py
+++ ex1.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
+anotherSurface = DISPLAYSURF.convert_alpha()
+anotherSurface.fill((128,0,0,250))
+pygame.draw.rect(anotherSurface, (0, 0, 200, 250), pygame.Rect(0, 0, 400, 300))
 pygame.display.set_caption('你好!')
 print(DISPLAYSURF)
 while True: # main game loop
